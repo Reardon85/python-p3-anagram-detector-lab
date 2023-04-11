0970f6c8b822d4b15033b6d40701df6fbc5ea656
Brian Reardon <brian.d.reardon@gmail.com>
2023-04-11 08:15:38 -0400

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+
+class Anagram:
+    
+    def __init__(self, word) -> None:
+        self.word = word
+
+    def match(self, the_list):
+        sorted_word = sorted(self.word)
+        return [x for x in the_list if sorted(x) == sorted_word]
